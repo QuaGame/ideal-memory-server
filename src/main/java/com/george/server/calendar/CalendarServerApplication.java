63b9ffdd043e59ee126e5e4a78e1f966265cea3c
George Filatov <georgyfilatov@yandex.ru>
2024-02-05 15:05:07 +0300

    init commit

---
+++ src/main/java/com/george/server/calendar/CalendarServerApplication.java
@@ -0,0 +1,13 @@
+package com.george.server.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/george/server/calendar/CalendarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.server.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
